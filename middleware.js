d52469497854e4fa5bea0770f5686053fdd898f7
Vishal Mehra <vishalmehra@vishals-MacBook-Air.local>
2024-12-05 15:41:10 +0530

    changes

--- middleware.js
+++ middleware.js
@@ -2,14 +2,12 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
 
-
-  return NextResponse.redirect('my.hiringplug.com');
-
   if (req.geo?.country === "CA" && req?.nextUrl?.host === process.env.NEXT_PUBLIC_BASE_URL) {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_CA_URL}${req.nextUrl.pathname}`);
   }else if (req.geo?.country === "IN" && req?.nextUrl?.host === process.env.NEXT_PUBLIC_BASE_URL) {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_IN_URL}${req.nextUrl.pathname}`);
-  }else {
-    return NextResponse.next();
+  }else if(req.nextUrl.host === 'https://hp-in-web.vercel.app/') {
+    return NextResponse.redirect("https://my.hiringplug.com");
   }
+  return NextResponse.next();
 }

e6750a6e3e22dfb7c45a052ea3dd37b37bfa5e19
Vishal Mehra <vishalmehra@vishals-MacBook-Air.local>
2024-12-05 15:13:20 +0530

    changes

--- middleware.js
+++ middleware.js
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  
+
   if (req.geo?.country === "CA" && req?.nextUrl?.host === process.env.NEXT_PUBLIC_BASE_URL) {
-    return NextResponse.redirect(process.env.NEXT_PUBLIC_CA_URL);
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_CA_URL}${req.nextUrl.pathname}`);
   }else if (req.geo?.country === "IN" && req?.nextUrl?.host === process.env.NEXT_PUBLIC_BASE_URL) {
-    return NextResponse.redirect(process.env.NEXT_PUBLIC_IN_URL);
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_IN_URL}${req.nextUrl.pathname}`);
   }else {
     return NextResponse.next();
   }

1c2307f6bf2aacb738b9c02904c5fc39780aa3d8
Vishal Mehra <vishalmehra@vishals-MacBook-Air.local>
2024-12-05 15:47:58 +0530

    changes

--- middleware.js
+++ middleware.js
@@ -6,7 +6,7 @@ export async function middleware(req) {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_CA_URL}${req.nextUrl.pathname}`);
   }else if (req.geo?.country === "IN" && req?.nextUrl?.host === process.env.NEXT_PUBLIC_BASE_URL) {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_IN_URL}${req.nextUrl.pathname}`);
-  }else if(req.nextUrl.host === 'https://hp-in-web.vercel.app/') {
+  }else if(req.nextUrl.host === 'https://hp-in-522xgg0rd-v642642s-projects.vercel.app/') {
     return NextResponse.redirect("https://my.hiringplug.com");
   }
   return NextResponse.next();

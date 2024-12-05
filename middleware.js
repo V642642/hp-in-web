8618b83b9fd381e5d7b6a05cd85be7014672029d
Vishal Mehra <vishalmehra@vishals-MacBook-Air.local>
2024-12-05 15:25:40 +0530

    changes

--- middleware.js
+++ middleware.js
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
 
+
+  return NextResponse.redirect('my.hiringplug.com');
+
   if (req.geo?.country === "CA" && req?.nextUrl?.host === process.env.NEXT_PUBLIC_BASE_URL) {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_CA_URL}${req.nextUrl.pathname}`);
   }else if (req.geo?.country === "IN" && req?.nextUrl?.host === process.env.NEXT_PUBLIC_BASE_URL) {
